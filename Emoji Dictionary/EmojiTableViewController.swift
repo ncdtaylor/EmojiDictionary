03a940ab527c5b7a438a85b51b00cb29a4b1a141
Nick Taylor <nick@t-m-s.co.uk>
2017-11-07 19:38:44 +0000

    Basic Emoji Array Implemented

--- Emoji Dictionary/EmojiTableViewController.swift
+++ Emoji Dictionary/EmojiTableViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class EmojiTableViewController: UITableViewController {
+    
+    var emojis = ["😀","👌","🙄","😘"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,15 +19,14 @@ class EmojiTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return emojis.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
 
-        // Configure the cell...
-
+        cell.textLabel?.text = emojis[indexPath.row]
         return cell
     }
 }
